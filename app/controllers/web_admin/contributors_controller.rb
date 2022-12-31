c065c3709a271ea092282365fa8fe11ecfdfaf1e
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-12-31 13:43:13 +0000

    add-find-fashion

--- app/controllers/web_admin/contributors_controller.rb
+++ app/controllers/web_admin/contributors_controller.rb
@@ -1,10 +1,27 @@
 class WebAdmin::ContributorsController < ApplicationController
   def index
+    @contributors=Contributor.all
   end
 
   def show
+    @contributor=Contributor.find(params[:id])
   end
 
   def edit
+    @contributor=Contributor.find(params[:id])
   end
+
+  def update
+    contributor=Contributor.find(params[:id])
+    contributor.update(contributor_paramas)
+    redirect_to web_admin_contributor_path
+
+  end
+
+  private
+
+  def contributor_paramas
+    params.require(:contributor).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :brand_name, :get_profile_image, :profile_image, :email, :postal_code, :address, :telephone_number)
+  end
+
 end
