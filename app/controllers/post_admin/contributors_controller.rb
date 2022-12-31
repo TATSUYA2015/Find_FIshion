ba29d3c5061d488a73b1a7aee3a96947557ff669
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-12-31 11:32:43 +0000

    add-find-fashion

--- app/controllers/post_admin/contributors_controller.rb
+++ app/controllers/post_admin/contributors_controller.rb
@@ -8,11 +8,14 @@ class PostAdmin::ContributorsController < ApplicationController
   end
 
   def update
+    contributor=Contributor.find(params[:id])
+    contributor.update(contributor_paramas)
+    redirect_to  post_admin_contributor_path(current_contributor.id)
   end
 
   private
 
   def contributor_paramas
-    params.require(:contributor).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :brand_name, :email, :postal_code, :address, :telephone_number)
+    params.require(:contributor).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :brand_name, :profile_image, :email, :postal_code, :address, :telephone_number)
   end
 end
