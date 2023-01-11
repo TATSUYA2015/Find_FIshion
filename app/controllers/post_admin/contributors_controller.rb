eb84e671fedf5122036299628589eb4469a786b8
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2023-01-11 15:35:41 +0000

    add-find-fashion

--- app/controllers/post_admin/contributors_controller.rb
+++ app/controllers/post_admin/contributors_controller.rb
@@ -2,7 +2,7 @@ class PostAdmin::ContributorsController < ApplicationController
   def show
     @contributor=Contributor.find(params[:id])
   end
-  
+
   def edit
     @contributor=Contributor.find(params[:id])
   end
@@ -13,6 +13,12 @@ class PostAdmin::ContributorsController < ApplicationController
     redirect_to  post_admin_contributor_path(current_contributor.id)
   end
 
+  def withdrawal
+    @contributor=Contributor.find(params[:id])
+    @contributor.update(is_deleted: true)
+    reset_session
+    redirect_to new_contributor_session_path
+  end
 
   private
 
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,18 +3,13 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
-    @contributor=Contributor.find(params[:id])
     @item=Item.find(params[:id])
   end
 
   private
 
-  def contributor_paramas
-    params.require(:contributor).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :brand_name, :get_profile_image, :profile_image, :email, :postal_code, :address, :telephone_number)
-  end
-
   def item_params
-    params.require(:item).permit(:genre_id, :name, :image, :introduction)
+    params.require(:item).permit(:contributor_id, :genre_id, :name, :image, :introduction)
   end
 
 end
